d0d4c2558c578382da17314ae3eeb58cb4b896ef
Nijisha <Nijisha@users.noreply.github.com>
2016-04-14 15:43:04 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,4 @@
 # Start web server
 #service httpd start
 docker pull nijisha/mt-mvn-vr-presentation:7.0.2
-docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-117IBEMF9LR9S-1453664566.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:7.0.2
+docker run --name web -d -p 8080:8080 -e BUSINESS_IP=http://VehicleRe-Business-18KXQJHMQNMW6-634711867.ap-southeast-2.elb.amazonaws.com/VehicleBusiness/vehiclerental nijisha/mt-mvn-vr-presentation:7.0.2
